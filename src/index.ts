dfd0187012f4b10d60ca2fed32fc1c13f46a281c
khushmohammad <khush311407@gmail.com>
2023-08-14 17:05:57 +0530

    router-controller

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Express, json } from "express";
+import { crud, user } from "./router";
+
+const app: Express = express();
+const port: number = 8080;
+app.use(json());
+app.use("/user", user);
+app.use("/crud", crud);
+
+app.listen(port, () =>
+  console.log("listening on port:http://localhost:" + port)
+);
